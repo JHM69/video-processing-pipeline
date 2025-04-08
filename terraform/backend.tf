9690705bbd49e8c6f31a7a82dd9efec80b113c10
Jahangir Hossain <jahangirhmarup@gmail.com>
2025-04-09 03:59:44 +0600

    Update Terraform backend region from us-west-2 to us-east-1 for consistency

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "mybuket65443"
     key            = "multi-cloud-lb/terraform.tfstate"
-    region         = "us-west-2"
+    region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-state-lock"
   }
